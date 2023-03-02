3740aa8dabb6789e6f896941dcf926a543e8125a
aninhaferreira <124297002+aninhaferreira@users.noreply.github.com>
2023-03-02 14:58:45 -0300

    Add files via upload

---
+++ TextFieldFrame/src/textfieldframe/TextFieldTest.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package textfieldframe;
+
+import javax.swing.JFrame;
+
+public class TextFieldTest {
+
+public static void main(String[] args) {
+
+TextFieldFrame textFieldFrame = new TextFieldFrame();
+textFieldFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+textFieldFrame.setSize(350, 100);
+textFieldFrame.setVisible(true);
+
+}
+
+}
